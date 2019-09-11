6586a4ff65ed7fefb4e88257f8c54cd776267e46
orlando1409 <orlando1409@hotmail.com>
2019-09-11 12:33:42 -0500

    updated CI

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker from CircleCI Environment";
+        return "Hello Docker from CircleCI Env";
     }
 
     public static void main(String[] args) {
